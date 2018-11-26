779d17b96121c9fe8a40d0cffe07c2b3ec29c5b7
Androidneter <15274873348@163.com>
2018-11-26 17:36:27 +0800

    Initial commit

---
+++ app/src/main/java/com/demo/www/MainActivity.java
@@ -0,0 +1,205 @@
+package com.demo.www;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.demo.www.db.RecordsDao;
+import com.zhy.view.flowlayout.FlowLayout;
+import com.zhy.view.flowlayout.TagAdapter;
+import com.zhy.view.flowlayout.TagFlowLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecordsDao mRecordsDao;
+    //默然展示词条个数
+    private final int DEFAULT_RECORD_NUMBER = 10;
+    private List<String> recordList = new ArrayList<>();
+    private TagAdapter mRecordsAdapter;
+    private LinearLayout mHistoryContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //默认账号
+        String username = "007";
+        //初始化数据库
+        mRecordsDao = new RecordsDao(this, username);
+
+        final EditText editText = findViewById(R.id.edit_query);
+        final TagFlowLayout tagFlowLayout = findViewById(R.id.fl_search_records);
+        final ImageView clearAllRecords = findViewById(R.id.clear_all_records);
+        final ImageView moreArrow = findViewById(R.id.iv_arrow);
+        TextView search = findViewById(R.id.iv_search);
+        ImageView clearSearch = findViewById(R.id.iv_clear_search);
+        mHistoryContent = findViewById(R.id.ll_history_content);
+
+        initData();
+
+        //创建历史标签适配器
+        //为标签设置对应的内容
+        mRecordsAdapter = new TagAdapter<String>(recordList) {
+
+            @Override
+            public View getView(FlowLayout parent, int position, String s) {
+                TextView tv = (TextView) LayoutInflater.from(MainActivity.this).inflate(R.layout.tv_history,
+                        tagFlowLayout, false);
+                //为标签设置对应的内容
+                tv.setText(s);
+                return tv;
+            }
+        };
+
+
+        tagFlowLayout.setAdapter(mRecordsAdapter);
+        tagFlowLayout.setOnTagClickListener(new TagFlowLayout.OnTagClickListener() {
+            @Override
+            public void onTagClick(View view, int position, FlowLayout parent) {
+                //清空editText之前的数据
+                editText.setText("");
+                //将获取到的字符串传到搜索结果界面,点击后搜索对应条目内容
+                editText.setText(recordList.get(position));
+                editText.setSelection(editText.length());
+            }
+        });
+        //删除某个条目
+        tagFlowLayout.setOnLongClickListener(new TagFlowLayout.OnLongClickListener() {
+            @Override
+            public void onLongClick(View view, final int position) {
+                showDialog("确定要删除该条历史记录？", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //删除某一条记录
+                        mRecordsDao.deleteRecord(recordList.get(position));
+                    }
+                });
+            }
+        });
+
+        //view加载完成时回调
+        tagFlowLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                boolean isOverFlow = tagFlowLayout.isOverFlow();
+                boolean isLimit = tagFlowLayout.isLimit();
+                if (isLimit && isOverFlow) {
+                    moreArrow.setVisibility(View.VISIBLE);
+                } else {
+                    moreArrow.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        moreArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tagFlowLayout.setLimit(false);
+                mRecordsAdapter.notifyDataChanged();
+            }
+        });
+
+        //清除所有记录
+        clearAllRecords.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog("确定要删除全部历史记录？", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        tagFlowLayout.setLimit(true);
+                        //清除所有数据
+                        mRecordsDao.deleteUsernameAllRecords();
+                    }
+                });
+            }
+        });
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String record = editText.getText().toString();
+                if (!TextUtils.isEmpty(record)) {
+                    //添加数据
+                    mRecordsDao.addRecords(record);
+                }
+            }
+        });
+
+        clearSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //清除搜索历史
+                editText.setText("");
+            }
+        });
+
+        mRecordsDao.setNotifyDataChanged(new RecordsDao.NotifyDataChanged() {
+            @Override
+            public void notifyDataChanged() {
+                initData();
+            }
+        });
+    }
+
+    private void showDialog(String dialogTitle, @NonNull DialogInterface.OnClickListener onClickListener) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage(dialogTitle);
+        builder.setPositiveButton("确定", onClickListener);
+        builder.setNegativeButton("取消", null);
+        builder.create().show();
+    }
+
+    private void initData() {
+        Observable.create(new ObservableOnSubscribe<List<String>>() {
+            @Override
+            public void subscribe(ObservableEmitter<List<String>> emitter) throws Exception {
+                emitter.onNext(mRecordsDao.getRecordsByNumber(DEFAULT_RECORD_NUMBER));
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<List<String>>() {
+                    @Override
+                    public void accept(List<String> s) throws Exception {
+                        recordList.clear();
+                        recordList = s;
+                        if (null == recordList || recordList.size() == 0) {
+                            mHistoryContent.setVisibility(View.GONE);
+                        } else {
+                            mHistoryContent.setVisibility(View.VISIBLE);
+                        }
+                        if (mRecordsAdapter != null) {
+                            mRecordsAdapter.setData(recordList);
+                            mRecordsAdapter.notifyDataChanged();
+                        }
+                    }
+                });
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        mRecordsDao.closeDatabase();
+        mRecordsDao.removeNotifyDataChanged();
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/demo/www/db/RecordSQLiteOpenHelper.java
@@ -0,0 +1,32 @@
+package com.demo.www.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Project：taobaohistory
+ * Author：zj
+ * Date：2018/11/14
+ * Version: 3.0.0
+ * Description：数据库帮助类
+ */
+public class RecordSQLiteOpenHelper extends SQLiteOpenHelper {
+    private final static String DB_NAME = "search_history.db";
+    private final static int DB_VERSION = 1;
+
+    public RecordSQLiteOpenHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sqlStr = "CREATE TABLE IF NOT EXISTS records (_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, keyword TEXT, time NOT NULL DEFAULT (datetime('now','localtime')));";
+        db.execSQL(sqlStr);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
